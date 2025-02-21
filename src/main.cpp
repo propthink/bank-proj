2622ec3ea2914a301ef37f3097928ccb8283e2f4
Joe <joewfanelli@gmail.com>
2025-02-20 20:59:38 -0500

    insert placeholders for transaction history and account management logic

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,65 @@
 //
+class Transaction
+{
+	//
+};
+
+//
+class TransactionNode
+{
+	//
+};
+
+//
+class TransactionHistory
+{
+	//
+};
+
+//
+class IAccount
+{
+	//
+};
+
+//
+class BAccount : public IAccount
+{
+	//
+};
+
+//
+class CheckingAccount : public BAccount
+{
+	//
+};
+
+//
+class SavingsAccount : public BAccount
+{
+	//
+};
+
+//
+class AccountNode
+{
+	//
+};
+
+//
+class AccountRegistry
+{
+	//
+};
+
+//
+class UserProfile
+{
+	//
+};
+
+//
+int main()
+{
+	//
+}
